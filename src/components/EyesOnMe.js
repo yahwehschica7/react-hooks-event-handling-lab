21105f7d45f0cc2a8107c2d67509568cd470cd4a
Jessica Nelson <yahwehschica@yahoo.com>
2022-01-27 12:39:01 -0500

    Assignment Complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
-// Code EyesOnMe Component Here
+
+
+function EyesOnMe() {
+    return (
+        <button
+        onFocus={(e) => {
+            console.log("Good!")
+        }}
+        onBlur={(e) => {
+            console.log('Hey! Eyes on me!');
+        }}
+        >Eyes on me    
+    </button>
+    )
+}
+
+
+
+
+export default EyesOnMe
+
+
+  
